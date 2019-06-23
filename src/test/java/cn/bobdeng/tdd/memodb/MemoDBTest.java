654a1cbd441ff1842bf067e0e6e6af059c8fa451
DengZhiGuo <bobdeng@163.com>
2019-06-23 14:07:34 +0800

    Default Changelist

--- src/test/java/cn/bobdeng/tdd/memodb/MemoDBTest.java
+++ src/test/java/cn/bobdeng/tdd/memodb/MemoDBTest.java
@@ -54,7 +54,15 @@ public class MemoDBTest {
     public void test_find() {
         memoDB.insert(new TestEntity("123","name"));
         memoDB.insert(new TestEntity("123","name"));
-        assertThat(memoDB.all().size()).isEqualTo(1);
+        assertThat(memoDB.all().size()).isEqualTo(2);
+
+    }
+    @Test
+    public void test_search() {
+        memoDB.insert(new TestEntity("123","name1"));
+        memoDB.insert(new TestEntity("112","name2"));
+        memoDB.insert(new TestEntity("113","hello"));
+       // assertThat(memoDB.search((entity)->entity.name.contains("name")).size()).isEqualTo(2);
 
     }
 }
